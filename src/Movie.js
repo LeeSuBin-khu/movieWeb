aae0e6dea2b91a0bc46fa4e053a640e6f9f80df9
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-09-11 22:23:38 +0900

    genre/poster 추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
-    return <h5>{title}</h5>
+function Movie({year, title, summary, poster, genres}){
+    return (
+    <div class="movies_movie">
+        <img src={poster} alt={title} title={title} />
+    <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <ul className="genres">
+            {genres.map((genre, index) => (
+            <li key={index} className="genres_genre">{genre}</li>
+            ))}
+        </ul>
+        <h5 class="movie_genre">{genres}</h5>
+        <p class="movie_summary">{summary}</p>
+    </div>
+</div>)
 }
 
 Movie.propTypes= {
@@ -10,7 +25,8 @@ Movie.propTypes= {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
